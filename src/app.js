c2dba94b70efbb94e9d37fcb6efce16be45af82a
acontrerasregueiro <acontrerasregueiro@gmail.com>
2022-03-29 07:19:02 +0000

    random card generator with javascript done

--- src/app.js
+++ src/app.js
@@ -1,11 +1,63 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+function getRandomArbitrary(min, max) {
+  return Math.floor(Math.random() * (max - min) + min);
+}
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+function generarPalo() {
+  let numero = getRandomArbitrary(1, 5);
+  //console.log(numero);
+  let palo,
+    color = "";
+  switch (numero) {
+    case 1: // corazones ♥
+      palo = "♥";
+      color = "red";
+      break;
+    case 2: //treboles ♣
+      palo = "♣";
+      color = "green";
+      break;
+    case 3: //picas ♠
+      palo = "♠";
+      color = "black";
+      break;
+    case 4: //diamantes ♦
+      palo = "♦";
+      color = "red";
+      break;
+  }
+  let string = palo + color;
+  return string;
+}
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+function darColor(color, carta) {
+  // console.log(color);
+  if (color == "red") {
+    document.getElementById(carta).style.color = "red";
+  } else if (color == "green") {
+    document.getElementById(carta).style.color = "green";
+  } else {
+    document.getElementById(carta).style.color = "black";
+  }
+}
+
+function pintarFiguras(numero) {
+  if (numero == 10) {
+    document.getElementById("numero").innerHTML = "J";
+  } else if (numero == 11) {
+    document.getElementById("numero").innerHTML = "Q";
+  } else if (numero == 12) {
+    document.getElementById("numero").innerHTML = "K";
+  }
+}
+
+renderizar();
+function renderizar() {
+  let palo = generarPalo();
+  let numero = getRandomArbitrary(1, 12);
+  document.getElementById("numero").innerHTML = numero;
+  pintarFiguras(numero);
+  document.getElementById("palo").innerHTML = palo[0];
+  document.getElementById("palodos").innerHTML = palo[0];
+
+  darColor(palo.substring(1), "carta");
+}
